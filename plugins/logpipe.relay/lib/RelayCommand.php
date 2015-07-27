d281cee591eb88598059fbe4cda8e56ae686ec45
Christopher Vagnetoft <cvagnetoft@gmail.com>
2015-07-27 21:53:19 +0200

    The relay plugin is now functional

--- plugins/logpipe.relay/lib/RelayCommand.php
+++ plugins/logpipe.relay/lib/RelayCommand.php
@@ -7,6 +7,8 @@ use NoccyLabs\LogPipe\Handler\LogPipeHandler;
 use Monolog\Logger;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputOption;
+use NoccyLabs\LogPipe\Transport\TransportFactory;
+use NoccyLabs\LogPipe\Posix\SignalListener;
 
 /**
  * Class LogTestCommand
@@ -22,7 +24,8 @@ class RelayCommand extends AbstractCommand
     {
         $this->setName("relay");
         $this->setDescription("Relay log data");
-        $this->addArgument("destination", InputArgument::IS_ARRAY, "Destination endpoints");
+        $this->addOption("endpoint", "e", InputOption::VALUE_REQUIRED, "The endpoint or pipe to dump", DEFAULT_ENDPOINT);
+        $this->addArgument("listeners", InputArgument::IS_ARRAY, "Destination listener endpoints");
     }
 
 
@@ -31,8 +34,50 @@ class RelayCommand extends AbstractCommand
      */
     protected function exec()
     {
-        $destinations = $this->input->getArgument("destination");
-        $this->output->writeln("Destination endpoints: <info>" . join(" ",$destinations) . "</info>");
+        $destinations = $this->input->getArgument("listeners");
+        if (count($destinations) == 0) {
+            $this->output->writeln("<error>You need to specify at least one listener!</error>");
+            return(1);
+        }
+
+        $this->output->writeln("Destination endpoints: <comment>" . join("</comment>, <comment>",$destinations) . "</comment>");
+
+        $destinationEndpoints = [];
+        foreach ($destinations as $destination) {
+            $endpoint = TransportFactory::create($destination);
+            $endpoint->connect();
+            $destinationEndpoints[] = $endpoint;
+        }
+        
+        $sourceEndpoint = TransportFactory::create($this->input->getOption("endpoint"));
+        $sourceEndpoint->listen();
+        
+        $signal = new SignalListener(SIGINT);
+        
+        $spin = "/-\\|";
+        $spix = 0;
+        $count = 0;
+        
+        $this->output->writeln("<info>Starting relay...</info>");
+        
+        declare(ticks=5);
+        while (true) {
+            do {
+                usleep(1000);
+                if ($signal()) {
+                    break(2);
+                }
+            } while (!($message = $sourceEndpoint->receive(false)));
+
+            if ($message) {            
+                foreach ($destinationEndpoints as $endpoint) {
+                    $endpoint->send($message);
+                }
+            }
+        }
+
+        $this->output->writeln("\n<info>Shutting down...</info>");
+        
     }
 
 }
